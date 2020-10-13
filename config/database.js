293f09cec4b16e1f3714b343c0e497431eaf4863
samyblm <sam.belambri@gmail.com>
2020-10-13 02:12:33 +0100

    add

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('esi_hub', 'root', 'node-projects', {
-    host:'localhost' ,
-    dialect: 'mysql',
-    port: 3306,
-    logging: false,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+const sequelize = new Sequelize("sam", "samy", "samy", {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306,
+  logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 module.exports = sequelize;
